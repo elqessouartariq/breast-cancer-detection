f86ff4601d348381508ba95eb0318d0433162d10
elqessouartariq <tariqelqessouar@gmail.com>
2024-03-11 17:51:06 +0000

    replace sidebar by streamlit_option_menu (navbar)

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 from Tabs import home, predict, visualise, about
 from web_functions import load_data
-
 
 # Configure the app
 st.set_page_config(
     page_title = 'Breast Cancer Detection',
     layout = 'wide',
     initial_sidebar_state = 'auto'
+)
+
+selected = option_menu(
+    menu_title = None,
+    options = ['Home','About', 'Prediction', 'Visualisation'],
+    icons=['bi-house','bi-info-circle','bi-calculator','bi-diagram-3'],
+    menu_icon='🔗',
+    default_index=0,
+    orientation='horizontal',
 )
 
 # Dictionary for pages
@@ -18,13 +27,11 @@ Tabs = {
     "Visualisation": visualise
 }
 
-st.sidebar.title("Navigation")
-
-page = st.sidebar.radio("Pages", list(Tabs.keys()))
+page = selected
 
 df, X, y = load_data()
 
-# Call the app funciton of selected page to run
+# Call the app function of the selected page to run
 if page in ["Prediction", "Visualisation"]:
     Tabs[page].app(df, X, y)
 elif (page == "Data Info"):
