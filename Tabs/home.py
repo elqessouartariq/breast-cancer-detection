96689e3ad05968fe17ece25a5e315423b8370825
elqessouartariq <tariqelqessouar@gmail.com>
2024-03-11 15:57:38 +0000

    Remove unnecessary code and comments

--- Tabs/home.py
+++ Tabs/home.py
@@ -1,14 +1,8 @@
-"""This modules contains data about home page"""
-
-# Import necessary modules
 import streamlit as st
 
 def app():
-    """This function create the home page"""
 
-    # Add title to the home page
     st.title("Breast Cancer Detection using Machine Learning")
 
-    # Add image to the home page
     st.image("./images/home.jpg")
 
