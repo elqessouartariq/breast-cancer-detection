e8b66abb6099911606ad270476ab5209a59f7e9c
elqessouartariq <tariqelqessouar@gmail.com>
2024-03-12 11:44:23 +0000

    Refactor user input layout in predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -4,22 +4,29 @@ from web_functions import predict
 def app(df, X, y):
 
     st.title("Detect Breast Cancer")
-    
+
     st.subheader("Select Values:")
 
-    # Take input of features from the user.
-    rad = st.number_input("Radius", float(df["radius_mean"].min()), float(df["radius_mean"].max()))
-    tex = st.number_input("Texture", float(df["texture_mean"].min()), float(df["texture_mean"].max()))
-    per = st.number_input("Perimeter", float(df["perimeter_mean"].min()), float(df["perimeter_mean"].max()))
-    are = st.number_input("Area", float(df["area_mean"].min()), float(df["area_mean"].max()))
-    smo = st.number_input("Smoothness", float(df["smoothness_mean"].min()), float(df["smoothness_mean"].max()))
-    com = st.number_input("Compactness", float(df["compactness_mean"].min()), float(df["compactness_mean"].max()))
-    con = st.number_input("Concavity", float(df["compactness_mean"].min()), float(df["compactness_mean"].max()))
-    sym = st.number_input("Symmetry", float(df["symmetry_mean"].min()), float(df["symmetry_mean"].max()))
-    fad = st.number_input("Fractal Dimension", float(df["fractal_dimension_mean"].min()), float(df["fractal_dimension_mean"].max()))
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        # Take input of features from the user.
+        rad = st.number_input("Radius", float(df["radius_mean"].min()), float(df["radius_mean"].max()))
+        tex = st.number_input("Texture", float(df["texture_mean"].min()), float(df["texture_mean"].max()))
+        per = st.number_input("Perimeter", float(df["perimeter_mean"].min()), float(df["perimeter_mean"].max()))
+
+    with col2:
+        are = st.number_input("Area", float(df["area_mean"].min()), float(df["area_mean"].max()))
+        smo = st.number_input("Smoothness", float(df["smoothness_mean"].min()), float(df["smoothness_mean"].max()))
+        com = st.number_input("Compactness", float(df["compactness_mean"].min()), float(df["compactness_mean"].max()))
+    with col3:
+        con = st.number_input("Concavity", float(df["compactness_mean"].min()), float(df["compactness_mean"].max()))
+        sym = st.number_input("Symmetry", float(df["symmetry_mean"].min()), float(df["symmetry_mean"].max()))
+        fad = st.number_input("Fractal Dimension", float(df["fractal_dimension_mean"].min()), float(df["fractal_dimension_mean"].max()))
 
     # Create a list to store all the features
     features = [rad,tex,per,are,smo,com,con,sym,fad]
+    print("feat:",features)
 
     # Create a button to predict
     if st.button("Detect"):
